15ee9a639364282b5a026456be4685a919c63c0f
Mike <abseander@gmail.com>
2023-03-02 13:16:21 +0100

    lot of backend logic added

--- Backend/ViergewinntManager.py
+++ Backend/ViergewinntManager.py
@@ -23,29 +23,50 @@ class GameManagement(GameObserver):
         self.player2_sessions = {}
 
     def fetch_state(self, token):
-
+        status = "ok"
         if token in self.player1_sessions:
             game = self.player1_sessions[token]
         elif token in self.player2_sessions:
             game = self.player2_sessions[token]
         else:
-            return "error"
+            status = "no game found"
 
         token1, token2 = self.get_tokens_by_game_id(game.id)
-        return game.State, user_manager.sessions[token1], user_manager.sessions[token2]
+        return status, game.State, user_manager.sessions[token1], user_manager.sessions[token2]
 
     def request_solo_game(self, token):
         game = Viergewinnt(self)
         self.player1_sessions[token] = game
         self.player2_sessions[token] = game
-
         token1, token2 = self.get_tokens_by_game_id(game.id)
-        return game.State, user_manager.sessions[token1], user_manager.sessions[token2]
+        return "ok", game.State, user_manager.sessions[token1], user_manager.sessions[token2]
 
     def make_move(self, token, move):
-        # check validity
-        # return vg4state
-        pass
+        status = "ok"
+        player1_moved = False
+        game = None
+        if token in self.player1_sessions:
+            game = self.player1_sessions[token]
+            if game.State.player1turn:
+                try:
+                    game.playerMadeMove(move)
+                    player1_moved = True
+                except:
+                    status = "bad move"
+        if token in self.player2_sessions and not player1_moved:
+            game = self.player2_sessions[token]
+            if not game.State.player1turn:
+                try:
+                    game.playerMadeMove(move)
+                except:
+                    status = "bad move"
+        if not game:
+            status = "there is no game going on"
+            return status, None, None, None
+        else:
+            token1, token2 = self.get_tokens_by_game_id(game.id)
+            return status, game.State, user_manager.sessions[token1], user_manager.sessions[token2]
+
 
     def fetch_invites(self):
         pass
--- Backend/flask_app.py
+++ Backend/flask_app.py
@@ -11,12 +11,17 @@ CORS(app)
 
 email = "username"
 password = "password"
-move = "coloumnNumber"
+json_move = "coloumnNumber"
 option = "option"
 token = "token"
 match_type = "match_type"
-
+status_name = "status"
 match_type_solo = "solo"
+json_player1turn = "player1turn"
+json_game_field = "game_field"
+json_player1 = "player1"
+json_player2 = "player2"
+json_game_state = "game_state"
 
 
 @app.route('/create_user', methods=['POST'])
@@ -30,15 +35,15 @@ def create_user():
             result = user_manager.add_user(x[email], x[password])
         else:
             result = "bad request"
-        if result == "invalid" or result == "bad request":
+        if result == "invalid" or result == "bad request" or result == "user already exists":
             response = {
                 token: 0,
-                "status": result
+                status_name: result
             }
         else:
             response = {
                 token: result,
-                "status": "ok"
+                status_name: "ok"
             }
         # response.headers.add('Access-Control-Allow-Origin', '*')
         return jsonify(response)
@@ -58,15 +63,29 @@ def login():
         if result == "invalid" or result == "bad request":
             response = {
                 token: 0,
-                "status": result
+                status_name: result
             }
         else:
             response = {
                 token: result,
-                "status": "ok"
+                status_name: "ok"
             }
-        # response.headers.add('Access-Control-Allow-Origin', '*')
         return jsonify(response)
+
+
+@app.route('/logout', methods=['POST'])
+@cross_origin()
+def logout():
+    if request.method == 'POST':
+        x = request.json
+        print(x)
+
+        if token in x:
+            user_manager.logout(x[token])
+
+        return jsonify({
+            status_name: "ok"
+        })
 
 
 @app.route('/state', methods=['GET'])
@@ -75,9 +94,23 @@ def get_game_state():
     if request.method == 'GET':
         x = request.json
         print(x)
+        answer = "there has been an issue"
         if token in x:
-            return user_manager.add_user(x[email], x[password])
-        return "bad request"
+            status, game_state, player1_name, player2_name = game_manager.fetch_state(x[token])
+        else:
+            status = "bad request"
+        if status == "ok":
+            return jsonify({
+                status_name: status,
+                json_player1turn: game_state.player1turn,
+                json_game_field: create_game_field_response(game_state.SpielFeld),
+                json_player1: player1_name,
+                json_player2: player2_name
+            })
+        else:
+            return jsonify({
+                status_name: status
+            })
 
 
 @app.route('/request_game', methods=['POST'])
@@ -89,9 +122,55 @@ def request_game():
         answer = "there has been an issue"
         if token in x and match_type in x:
             if x[match_type] == match_type_solo:
-                answer = game_manager.request_game(x[token], x[match_type])
+                status, game_state, player1_name, player2_name = game_manager.request_solo_game(x[token])
+            else:
+                status = "unkown match type"
+        else:
+            status = "bad request"
+        if status == "ok":
+            return jsonify({
+                status_name: status,
+                json_player1turn: game_state.player1turn,
+                json_game_field: create_game_field_response(game_state.SpielFeld),
+                json_player1: player1_name,
+                json_player2: player2_name
+            })
+        else:
+            return jsonify({
+                status_name: status
+            })
 
-        return answer
+
+@app.route('/move', methods=['POST'])
+@cross_origin()
+def move():
+    if request.method == 'POST':
+        x = request.json
+        print(x)
+        answer = "there has been an issue"
+        if token in x and json_move in x:
+            status, game_state, player1_name, player2_name = game_manager.make_move(x[token], x[json_move])
+        else:
+            status = "bad request"
+        if status == "ok":
+            return jsonify({
+                status_name: status,
+                json_player1turn: game_state.player1turn,
+                json_game_field: create_game_field_response(game_state.SpielFeld),
+                json_player1: player1_name,
+                json_player2: player2_name
+            })
+        else:
+            return jsonify({
+                status_name: status
+            })
+
+
+def create_game_field_response(game_field):
+    result = {}
+    for index, coloumn in enumerate(game_field):
+        result["coloumn " + str(index)] = coloumn
+    return result
 
 
 if __name__ == '__main__':
