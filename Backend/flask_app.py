99762467d2c9e6e07f1946fe18c8c4654c453eae
Daniel Gruca <daniel.gruca@parimeo.de>
2023-03-04 19:22:43 +0100

    flask frontend refactoring

--- Backend/flask_app.py
+++ Backend/flask_app.py
@@ -10,19 +10,22 @@ from user_management import UserManagement, user_manager
 app = Flask(__name__)
 CORS(app)
 
-email = "username"
-password = "password"
-json_move = "coloumnNumber"
-option = "option"
-token = "token"
-match_type = "match_type"
-status_name = "status"
-match_type_solo = "solo"
-json_player1turn = "player1turn"
-json_game_field = "game_field"
-json_player1 = "player1"
-json_player2 = "player2"
-json_game_finish = "game_state"
+
+class Response:
+
+    email = "username"
+    password = "password"
+    json_move = "coloumnNumber"
+    option = "option"
+    token = "token"
+    match_type = "match_type"
+    status_name = "status"
+    match_type_solo = "solo"
+    json_player1turn = "player1turn"
+    json_game_field = "game_field"
+    json_player1 = "player1"
+    json_player2 = "player2"
+    json_game_finish = "game_state"
 
 
 class BadRequestException(Exception):
@@ -32,13 +35,13 @@ class BadRequestException(Exception):
 @app.route('/create_user', methods=['POST'])
 @cross_origin()
 def create_user():
-    validation, properties = validate_request([email, password], request)
+    validation, properties = validate_request([Response.email, Response.password], request)
     if validation == "ok":
 
         status, result = user_manager.add_user(*properties)
         response = {
-            token: result,
-            status_name: status
+            Response.token: result,
+            Response.status_name: status
         }
 
         # response.headers.add('Access-Control-Allow-Origin', '*')
@@ -48,13 +51,13 @@ def create_user():
 @app.route('/login', methods=['POST'])
 @cross_origin()
 def login():
-    validation, properties = validate_request([email, password], request)
+    validation, properties = validate_request([Response.email, Response.password], request)
     if validation == "ok":
         status, result = user_manager.login(*properties)
 
         response = {
-            token: result,
-            status_name: status
+            Response.token: result,
+            Response.status_name: status
         }
         return jsonify(response)
 
@@ -62,84 +65,84 @@ def login():
 @app.route('/logout', methods=['POST'])
 @cross_origin()
 def logout():
-    validation, properties = validate_request([token], request)
+    validation, properties = validate_request([Response.token], request)
     if validation == "ok":
         user_manager.logout(*properties)
         return jsonify({
-            status_name: "ok"
+            Response.status_name: "ok"
         })
 
 
 @app.route('/get_profile', methods=['GET'])
 @cross_origin()
 def get_profile():
-    validation, properties = validate_request([token, email], request)
+    validation, properties = validate_request([Response.token, Response.email], request)
     if validation == "ok":
         user_manager.get_user_profile(*properties)
         return jsonify({
-            status_name: "ok"
+            Response.status_name: "ok"
         })
 
 
 @app.route('/state', methods=['GET'])
 @cross_origin()
 def get_game_state():
-    validation, properties = validate_request([token], request)
+    validation, properties = validate_request([Response.token], request)
     if validation == "ok":
         status, game_state, player1_name, player2_name = game_manager.fetch_state(*properties)
         response = create_game_state(game_state, player1_name, player2_name)
-        response[status_name] = status
+        response[Response.status_name] = status
         return jsonify(response)
 
     else:
         return jsonify({
-            status_name: validation
+            Response.status_name: validation
         })
 
 
 @app.route('/request_game', methods=['POST'])
 @cross_origin()
 def request_game():
-    validation, properties = validate_request([token, match_type], request)
+    validation, properties = validate_request([Response.token, Response.match_type], request)
     if validation == "ok":
         r_token, r_match_type = properties
         response = {}
-        if r_match_type == match_type_solo:
+        if r_match_type == Response.match_type_solo:
             status, game_state, player1_name, player2_name = game_manager.request_solo_game(r_token)
             response = create_game_state(game_state, player1_name, player2_name)
         else:
             status = "unknown match type"
 
-        response[status_name] = status
+        response[Response.status_name] = status
         return jsonify(response)
     else:
         return jsonify({
-            status_name: validation
+            Response.status_name: validation
         })
 
 
 @app.route('/move', methods=['POST'])
 @cross_origin()
 def move():
-    validation, properties = validate_request([token, json_move], request)
+    validation, properties = validate_request([Response.token, Response.json_move], request)
     if validation == "ok":
         status, game_state, player1_name, player2_name = game_manager.make_move(*properties)
         response = create_game_state(game_state, player1_name, player2_name)
-        response[status_name] = status
+        response[Response.status_name] = status
         return jsonify(response)
     else:
         return jsonify({
-            status_name: validation
+            Response.status_name: validation
         })
 
 
 def create_game_state(game_state: Status4G, player1_name, player2_name):
     return {
-        json_player1turn: game_state.player1turn,
-        json_game_field: create_game_field_response(game_state.SpielFeld),
-        json_game_finish: game_state.result,
-        json_player1: player1_name,
-        json_player2: player2_name
+        Response.json_player1turn: game_state.player1turn,
+        Response.json_game_field: create_game_field_response(game_state.SpielFeld),
+        Response.json_game_finish: game_state.result,
+        Response.json_player1: player1_name,
+        Response.json_player2: player2_name
     }
 
 
@@ -154,8 +157,8 @@ def validate_request(args: list, request_to_validate):
     json = request_to_validate.json
     props = find_properties_in_answer(args, json)
     status = "ok"
-    if token in args:
-        if not user_manager.validate_token(props[args.index(token)]):
+    if Response.token in args:
+        if not user_manager.validate_token(props[args.index(Response.token)]):
             status = "bad request"
     if not props:
         status = "bad request"
